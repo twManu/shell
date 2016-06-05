5eaad63fe6d76e7af9b53a131bdc54bd35037c99
Manu Chen <manuchen@gmail.com>
2016-06-05 16:22:07 +0800

    adjust installation items

--- rc/setup_am57.sh
+++ rc/setup_am57.sh
@@ -2,7 +2,7 @@
 
 #for host
 #
-#sudo apt-get install xfce4 xfce4-terminal virt-manager libvirt-bin qemu-system qemu-kvm linux-image-extra-virtual minicom
+#sudo apt-get install virt-manager libvirt-bin qemu-system qemu-kvm linux-image-extra-virtual
 #sudo echo 'auto eth0' >>/etc/network/interfaces
 #sudo echo 'iface eth0 inet manual' >>/etc/network/interfaces
 #sudo echo 'auto br0' >>/etc/network/interfaces
@@ -12,8 +12,8 @@
 #sudo echo '\tbridge_fd 0' >>/etc/network/interfaces
 #sudo echo '\tbridge_maxwait 0' >>/etc/network/interfaces
 
+sudo apt-get install -y xfce4 xfce4-terminal minicom nfs-common nfs-kernel-server
 sudo apt-get install -y filezilla vsftpd tftpd-hpa g++ vim git gitk\
 	synaptic ssh meld dos2unix
-sudo apt-get install -y nfs-common nfs-kernel-server
 sudo echo '/home/aten-3a00/ti *(rw,sync,no_subtree_check)' >>/etc/exports
 echo 'export EDITOR=vim' >>~/.bashrc
