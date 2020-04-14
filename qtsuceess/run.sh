c7f3a8317983e89bbf4e78791f5c1e4d9184bb47
ManuChen <manuchen@gmail.com>
2020-04-14 10:15:56 +0800

    add test

--- qtsuceess/run.sh
+++ qtsuceess/run.sh
@@ -6,7 +6,13 @@
 # sudo symlinks -c -r /
 # delete config.cache
 
+#test this
+./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license\
+ -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu-\
+ -sysroot / -nomake tests -prefix /usr/local/qt5 -extprefix /usr/local/qt5 -hostprefix /usr/local/qt5 -skip webview -skip qtwebengine -opengl es2
 
+
+#works
 ./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=/home/manu/bin/gcc-linaro-6.5.0-2018.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- -sysroot /home/manu/jetson -nomake examples -nomake tests -prefix /usr/local/qt5 -extprefix /home/manu/Downloads/qt-everywhere-src-5.14.1/NanoR32.3.1/qt5 -hostprefix /home/manu/Downloads/qt-everywhere-src-5.14.1/NanoR32.3.1/qt5-host -skip webview -skip qtwebengine -opengl es2
 
 #not this
