56e40250b11d9ee60b931b4b022ad522a3790e86
manu <manu@manu>
2020-04-14 16:05:01 +0800

    update

--- qtsuceess/nano/run.sh
+++ qtsuceess/nano/run.sh
@@ -11,9 +11,10 @@
 # cd GLES; ar x ../libgles2*.deb; tar xf data.tar.xz
 # cd EGLS; ar x ../libegl1*.deb; tar xf data.tar.xz
 # copy to /usr/include
+# sudo usermod -a -G input manu //reboot takes effect logout?
 
-#nano test
-./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -sysroot / -nomake examples -nomake tests -prefix /usr/lib/qt5.14.1 -extprefix /home/manu/Documents/qt5 -hostprefix /home/manu/Documents/qt5-host -skip webview -skip qtwebengine -opengl es2
+#nano works
+./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -sysroot / -nomake tests -prefix /usr/local/qt5 -extprefix /usr/local/qt5 -hostprefix /usr/local/qt5 -skip webview -skip qtwebengine -opengl es2
 
 #nano fail
 ./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -nomake examples -nomake tests -prefix /usr/lib/qt5.14.1 -skip webview -skip qtwebengine -opengl es2
