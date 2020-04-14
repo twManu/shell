1ebb0fef01b2bc6aad5f52cc3fc4115dd1971c8f
manu <manu@manu>
2020-04-14 16:41:44 +0800

    update

--- qtsuceess/nano/run.sh
+++ qtsuceess/nano/run.sh
@@ -12,6 +12,12 @@
 # cd EGLS; ar x ../libegl1*.deb; tar xf data.tar.xz
 # copy to /usr/include
 # sudo usermod -a -G input manu //reboot takes effect logout?
+# then
+# add .bashrc
+#QT_PATH=/usr/local/qt5
+#export PATH=${QT_PATH}/bin:$PATH
+#export LD_LIBRARY_PATH=${QT_PATH}/lib:${LD_LIBRARY_PATH}
+
 
 #nano works
 ./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -sysroot / -nomake tests -prefix /usr/local/qt5 -extprefix /usr/local/qt5 -hostprefix /usr/local/qt5 -skip webview -skip qtwebengine -opengl es2
