367f70b9e4f7b1fc1d2f6861817972d992cd30c0
manu <manu@home>
2020-05-01 20:27:36 +0800

    update

--- qtsuceess/nano/run.sh
+++ qtsuceess/nano/run.sh
@@ -9,18 +9,21 @@
 # apt download libgles2-mesa-dev libegl1-mesa-dev
 # mkdir GLES EGL
 # cd GLES; ar x ../libgles2*.deb; tar xf data.tar.xz
-# cd EGLS; ar x ../libegl1*.deb; tar xf data.tar.xz
+# cd EGL; ar x ../libegl1*.deb; tar xf data.tar.xz
 # copy to /usr/include
 # sudo usermod -a -G input manu //reboot takes effect logout?
 # then
 # add .bashrc
+# *** modify qtbase/mkspecs/devices/linux-jetson-tx1-g++/qmake.conf
+# QMAKE_INCDIR_POST remove /usr/include
+# ***
 #QT_PATH=/usr/local/qt5
 #export PATH=${QT_PATH}/bin:$PATH
 #export LD_LIBRARY_PATH=${QT_PATH}/lib:${LD_LIBRARY_PATH}
 
 
 #nano works
-./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -sysroot / -nomake tests -prefix /usr/local/qt5 -extprefix /usr/local/qt5 -hostprefix /usr/local/qt5 -skip webview -skip qtwebengine -opengl es2
+./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -prefix /usr/local/qt5 -extprefix /usr/local/qt5 -hostprefix /usr/local/qt5 -skip webview -skip qtwebengine -opengl es2
 
 #nano fail
 ./configure -shared -c++std c++14 -opensource -release -recheck-all --confirm-license -device linux-jetson-tx1-g++ -device-option CROSS_COMPILE=aarch64-linux-gnu- -nomake examples -nomake tests -prefix /usr/lib/qt5.14.1 -skip webview -skip qtwebengine -opengl es2
