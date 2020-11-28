35c6a9e7fa9f03ec8e0d75a4036fdb7fe499ff1a
manu <manu@home>
2020-11-28 15:59:13 +0800

    fix: missing xcb for input

--- qtsuceess/nano/device.sh
+++ qtsuceess/nano/device.sh
@@ -5,5 +5,5 @@ pushd GLES; ar x ../libgles2*.deb; tar xf data.tar.xz; sudo cp -a usr/include/GL
 pushd EGL; ar x ../libegl1*.deb; tar xf data.tar.xz; sudo cp -a usr/include/EGL /usr/include; popd
 cp -a /usr/src/nvidia/graphics_demos/include/KHR /usr/include
 
-sudo apt install libglm-dev libinput-dev libudev-dev symlinks
+sudo apt install libglm-dev libinput-dev libudev-dev symlinks ".*libxcb.*" libxrender-dev libxi-dev libfontconfig1-dev
 pushd /; sudo symlinks -c -r / ;tar cjf ~/nano441_root.tgz lib usr/include usr/lib usr/local/cuda*; popd
