56adc5ccabed67546845bf702c2e859f37b613a4
manu <manu@home>
2021-10-09 09:34:36 +0800

    more example

--- udev.sh
+++ udev.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "udevadm info --query=all --name=usb"
+echo "udevadm info --query=all --name=/dev/dri/card0 --attribute-walk"
+echo "udevadm info --attribute-walk --path=$(udevadm info --query=path --name=/dev/dri/card0)"
 echo "udevadm monitor"
+echo "sudo udevadm control --reload"
+echo "tail -f /var/log/usbdevice.log"
