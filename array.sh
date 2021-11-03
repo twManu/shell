50f362ff38f16418bae8a85a8d399367674b75b1
Manu Chen <manu.chen@aristaipc.com>
2021-11-03 16:35:00 +0800

    more array access

--- array.sh
+++ array.sh
@@ -4,3 +4,5 @@ my_array=(abc def ghi)
 
 for ((i=0; i<${#my_array[@]}; i++ )); do echo ${my_array[$i]}; done
 echo ${my_array[*]}
+
+for ((i=0; i<${#my_array[@]}; i++ )); do echo ${my_array[i]}; done
