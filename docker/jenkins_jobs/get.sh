4104979b5f1e0fc67976dc0ceb24645c49776b56
manu <manu@home>
2021-12-23 09:20:08 +0800

    update

--- docker/jenkins_jobs/get.sh
+++ docker/jenkins_jobs/get.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+# run after
+# docker exec -it mj /bin/bash
 
 tasks=(
 	"check_space"
+	"copy_to_release"
+	"edge_be"
+	"edge_branch"
+	"edge_fe"
+	"edge_pipe"
+	"edge_sys"
+	"edge_system"
 	"q8_buildroot"
 	"q8_client"
 	"q8_pipe"
 	"q8_server"
+	"release_to_version"
 )
 
+#edge_sys and edge_system deprecate
 
 for ((i=0; i<${#tasks[@]}; i++)); do
 	echo Getting task ${tasks[i]}
